ad593d1f13c97e269c29be88ebdb9b63fa35f63e
Varis Darasirikul <varis.dara@gmail.com>
2018-09-11 00:43:16 +0800

    Init

---
+++ app/src/main/java/com/amusexd/newsreader/MainActivity.java
@@ -0,0 +1,186 @@
+package com.amusexd.newsreader;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayList<String> content = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+
+    SQLiteDatabase articleDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+        listView.setAdapter(arrayAdapter);
+
+        articleDB = this.openOrCreateDatabase("Articles", MODE_PRIVATE, null);
+        articleDB.execSQL("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, articleId INTEGER, title VARCHAR, content VARCHAR)");
+
+        updateListView();
+
+        DownloadTask task = new DownloadTask();
+
+        try {
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateListView() {
+        Log.i("Listview ","Update List view");
+        Cursor c = articleDB.rawQuery("SELECT * FROM articles", null);
+        int contentIndex = c.getColumnIndex("content");
+        int titleIndex = c.getColumnIndex("title");
+
+        if (c.moveToFirst()) {
+            Log.i("moveToFirst ","moveToFirst");
+            titles.clear();
+            content.clear();
+
+            do {
+                titles.add(c.getString(titleIndex));
+                content.add(c.getString(contentIndex));
+            } while (c.moveToNext());
+
+            arrayAdapter.notifyDataSetChanged();
+        } else {
+            Log.i("moveToFirst ","Not moveToFirst");
+        }
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                Log.i("Url Content", result);
+
+                JSONArray jsonArray = new JSONArray(result);
+
+                int numberOfItems = 20;
+
+                if (jsonArray.length() < 20) {
+                    numberOfItems = jsonArray.length();
+                }
+
+                articleDB.execSQL("DELETE FROM articles");
+
+                Log.i("Print numberOfItems", numberOfItems + ": numberOfItems");
+
+                for (int i = 0; i < numberOfItems; i++) {
+                    Log.i("Print Current", i + ": i");
+                    // Log.i("JSON ITEM", jsonArray.getString(i));
+                    String articleId = jsonArray.getString(i);
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/" + articleId + ".json?print=pretty");
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    in = urlConnection.getInputStream();
+                    reader = new InputStreamReader(in);
+                    data = reader.read();
+                    String articleInfo = "";
+
+                    while (data != -1) {
+                        char current = (char) data;
+                        articleInfo += current;
+                        data = reader.read();
+                    }
+
+                    Log.i("article info", articleInfo);
+
+                    JSONObject jsonObject = new JSONObject(articleInfo);
+                    Log.i("article info Object", "Bollean" + jsonObject.isNull("title"));
+
+                    if (!jsonObject.isNull("title")) {
+                        String articleTitle = jsonObject.getString("title");
+                        String articleURL = jsonObject.getString("url");
+
+                        // Log.i("article info", articleTitle + articleURL);
+
+                        url = new URL(articleURL);
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        in = urlConnection.getInputStream();
+                        reader = new InputStreamReader(in);
+                        data = reader.read();
+                        String articleContent = "";
+
+                        while (data != -1) {
+                            char current = (char) data;
+                            articleContent += current;
+                            data = reader.read();
+                        }
+
+                        Log.i("article content", articleContent);
+
+                        String sql = "INSERT INTO articles (articleId, title, content) VALUES (?, ?, ?)";
+                        SQLiteStatement statement = articleDB.compileStatement(sql);
+
+                        statement.bindString(1, articleId);
+                        statement.bindString(2, articleTitle);
+                        statement.bindString(3, articleContent);
+
+                        statement.execute();
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            updateListView();
+        }
+    }
+
+}
