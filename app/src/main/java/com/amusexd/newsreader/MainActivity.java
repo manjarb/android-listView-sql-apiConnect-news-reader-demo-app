a046d2b341d922361557c487cfb78546da646599
Varis Darasirikul <varis.dara@gmail.com>
2018-09-11 00:53:21 +0800

    Init

--- app/src/main/java/com/amusexd/newsreader/MainActivity.java
+++ app/src/main/java/com/amusexd/newsreader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amusexd.newsreader;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
@@ -7,6 +8,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -39,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
 
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ArticlesActivity.class);
+                intent.putExtra("content", content.get(position));
+                startActivity(intent);
+            }
+        });
 
         articleDB = this.openOrCreateDatabase("Articles", MODE_PRIVATE, null);
         articleDB.execSQL("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, articleId INTEGER, title VARCHAR, content VARCHAR)");
@@ -48,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         DownloadTask task = new DownloadTask();
 
         try {
-            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+            // task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
         } catch (Exception e) {
             e.printStackTrace();
         }
